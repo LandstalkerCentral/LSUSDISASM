2422cd8a31d3c4d487a0acc8d59eeef45a05ff61
Wiz <the_wizard_who_says_ni@hotmail.com>
2020-02-27 00:03:17 +0100

    Map tilesets split along with moved map palettes.

---
+++ disasm/data/graphics/maps/tilesets/animated/entries.asm
@@ -0,0 +1,25 @@
+
+; ASM FILE data\graphics\maps\tilesets\animated\entries.asm :
+; 0x44014..0x4406C : Animated Map Tileset Pointer Table
+                dc.l AnimatedMapTileset00
+                dc.l AnimatedMapTileset01
+                dc.l AnimatedMapTileset02
+                dc.l AnimatedMapTileset03
+                dc.l AnimatedMapTileset04
+                dc.l AnimatedMapTileset05
+                dc.l AnimatedMapTileset06
+                dc.l AnimatedMapTileset07
+                dc.l AnimatedMapTileset08
+                dc.l AnimatedMapTileset09
+                dc.l AnimatedMapTileset10
+                dc.l AnimatedMapTileset11
+                dc.l AnimatedMapTileset12
+                dc.l AnimatedMapTileset13
+                dc.l AnimatedMapTileset14
+                dc.l AnimatedMapTileset15
+                dc.l AnimatedMapTileset16
+                dc.l AnimatedMapTileset17
+                dc.l AnimatedMapTileset18
+                dc.l AnimatedMapTileset19
+                dc.l AnimatedMapTileset20
+                dc.l AnimatedMapTileset21
---
+++ disasm/data/graphics/maps/tilesets/animated/storage.asm
@@ -0,0 +1,47 @@
+
+; ASM FILE data\graphics\maps\tilesets\animated\storage.asm :
+; 0x94F2A..0x9A4EA : Map Tileset Entries
+AnimatedMapTileset00:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset00.bin"
+AnimatedMapTileset01:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset01.bin"
+AnimatedMapTileset02:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset02.bin"
+AnimatedMapTileset03:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset03.bin"
+AnimatedMapTileset04:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset04.bin"
+AnimatedMapTileset05:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset05.bin"
+AnimatedMapTileset06:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset06.bin"
+AnimatedMapTileset07:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset07.bin"
+AnimatedMapTileset08:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset08.bin"
+AnimatedMapTileset09:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset09.bin"
+AnimatedMapTileset10:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset10.bin"
+AnimatedMapTileset11:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset11.bin"
+AnimatedMapTileset12:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset12.bin"
+AnimatedMapTileset13:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset13.bin"
+AnimatedMapTileset14:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset14.bin"
+AnimatedMapTileset15:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset15.bin"
+AnimatedMapTileset16:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset16.bin"
+AnimatedMapTileset17:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset17.bin"
+AnimatedMapTileset18:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset18.bin"
+AnimatedMapTileset19:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset19.bin"
+AnimatedMapTileset20:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset20.bin"
+AnimatedMapTileset21:
+                incbin "data/graphics/maps/tilesets/animated//animatedmaptileset21.bin"
